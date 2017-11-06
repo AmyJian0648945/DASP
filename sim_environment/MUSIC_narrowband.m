16311ee061180e871d72ec267c659e1284bc958b
AmyJian0648945 <amychien314159@gmail.com>
2017-11-06 16:07:40 +0100

    DOA estimation is less than 4!
    deleted the sampling frequency component in the calculation of tau,
    because if we added that in, then tau would’ve been unitless

--- sim_environment/MUSIC_narrowband.m
+++ sim_environment/MUSIC_narrowband.m
@@ -29,14 +29,14 @@ end
 % DFT window length L = 1024, and 50% overlap (each column contains stft estimate of each window) 
 L = 1024;
 for i=1:1:numOfMics
-    [stftMat(:,:,i), freq, ~, psd(:,:,i)] = spectrogram(mic(:,i), hamming(L),  [], L, computed_rir.fs_RIR, 'psd');
+    [stftMat(:,:,i), freq, ~, psd(:,:,i)] = spectrogram(mic(:,i), L, [], 2.*L, computed_rir.fs_RIR, 'psd');
 end
 
 % Averaged over time and mic, find the max power; index is the frequency
 % bin with the highest power
 psd = mean(mean(psd, 2), 3); % avg over diff. frames & mic
 [~, indexMaxFreq] = max(psd);
-wMax = freq(indexMaxFreq);
+wMax = freq(indexMaxFreq) .* 2 .* pi;
 
 
 
@@ -71,6 +71,7 @@ E(:,maxEigVal) = [];
 
 dm = zeros(numOfMics,1);
 theta = 0 : 0.5 : 180;
+theta = deg2rad(theta);
 c = 340; % [m/s]
 
 
@@ -85,10 +86,12 @@ end
 
 
 
+
 % Find tau(TDOA) function for each mic
-tau = dm .* computed_rir.fs_RIR .* cos(theta) ./ c;
-if(sourcePos == up) 
-	tau = -tau;
+if(sourcePos == up)
+	tau = -dm .* cos(theta) ./ c;
+else
+	tau = -dm .* cos(theta) ./ c;
 end
 
 % Create g(w=max freq bin, theta)
@@ -101,7 +104,7 @@ g = exp(-1i .* wMax .* tau);
 
 % Find the true DOA
 pw = diag(g'*E*E'*g).^-1; % should be scalar
-[~,indexTrueDOA] = max(pw);
+[~,indexTrueDOA] = max(abs(pw));
 DOA_est = (indexTrueDOA-1)./2;
 
 % Plot the pseudospectrum
