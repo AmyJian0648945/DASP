4227c986efcf198e30f1db32c61d2e2b78e71014
MatthieuFontaine <matthieu.fontaine@student.uclouvain.be>
2017-12-14 20:36:29 +0100

    Small motifs

--- sim_environment/crosscancel.m
+++ sim_environment/crosscancel.m
@@ -1,10 +1,10 @@
 clear all;
 load('Computed_RIRs.mat');
-computed_rir = load('Computed_RIRs.mat'); 
+computed_rir = load('Computed_RIRs.mat');
 load('HRTF.mat');
 %% Variables Init
 lenMicSig = 10; % 10 sec of speech
-fs_resample = 8e3; % sampling frequency 
+fs_resample = 8e3; % sampling frequency
 samplesToKeep = fs_resample.*lenMicSig;
 
 RIR_length = 1600;
@@ -21,46 +21,71 @@ source_speechB{1,1} = resample(source_speechB{1,1},fs_resample,source_speechB{1,
 source_speech1B = source_speechB{1,1}(1:samplesToKeep); %% truncation
 
 %%
-RIR_sources = RIR_sources(1:RIR_length,:,:); %% truncating 1500 samples 
+RIR_sources = RIR_sources(1:RIR_length,:,:); %% truncating 1500 samples
 Lh = size(RIR_sources,1);
 M = size(s_pos,1); %% number of loudspeakers
 Lg = ceil(2*(Lh-1)./(M-2)); %% condition for having same amount of equations as of unknowns
 
 
 temp = RIR_sources(:,mic_synth(2),1);
-n = 4;
+n = 7;
 switch n
     case 1
         xL = zeros(Lg+Lh-1,1); xL(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
         xR = zeros(Lg+Lh-1,1); xR(1:RIR_length) = temp;
-		
-		xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
         xRB = zeros(Lg+Lh-1,1); xRB(1:RIR_length) = temp;
         
     case 2
         xL = zeros(Lg+Lh-1,1); xL(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
         xR = zeros(Lg+Lh-1,1); xR(1:RIR_length) = 0.1.*temp;
-		
-		xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = 0.1.*RIR_sources(:,mic_synth(1),1);
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = 0.1.*RIR_sources(:,mic_synth(1),1);
         xRB = zeros(Lg+Lh-1,1); xRB(1:RIR_length) = RIR_sources(:,mic_synth(2),1);
         
     case 3
         xL = zeros(Lg+Lh-1,1); xL(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
         xR = zeros(Lg+Lh-1,1); xR(1:RIR_length) = [zeros(10,1); temp(1:end-10)];
-		
-		xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = [zeros(10,1); RIR_sources(1:end-10,mic_synth(1),1);];  
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = [zeros(10,1); RIR_sources(1:end-10,mic_synth(1),1);];
         xRB = zeros(Lg+Lh-1,1); xRB(1:RIR_length) = RIR_sources(:,mic_synth(2),1);
-		
+        
     case 4
         xL = zeros(Lg+Lh-1,1); xL(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
         xR = zeros(Lg+Lh-1,1); xR(1:RIR_length) = RIR_sources(:,mic_synth(2),1);
         xL = fftfilt(HRTF(:,1),xL,Lg+Lh-1);
         xR = fftfilt(HRTF(:,2),xR,Lg+Lh-1);
-		
-		xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1:RIR_length) = RIR_sources(:,mic_synth(1),1);
         xRB = zeros(Lg+Lh-1,1); xRB(1:RIR_length) = RIR_sources(:,mic_synth(2),1);
         xLB = fftfilt(HRTF(:,2),xLB,Lg+Lh-1);
         xRB = fftfilt(HRTF(:,1),xRB,Lg+Lh-1);
+    case 5
+        xL = zeros(Lg+Lh-1,1); xL(1) = 1;
+        xR = zeros(Lg+Lh-1,1); xR(1) = 1;
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1) =1;
+        xRB = zeros(Lg+Lh-1,1); xRB(1) = 1;
+    case 6
+        xL = zeros(Lg+Lh-1,1); xL(1) = 1;
+        xR = zeros(Lg+Lh-1,1); xR(1) = 0.5; %% attenuation ILD
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1) =0.5; %% attenuation ILD
+        xRB = zeros(Lg+Lh-1,1); xRB(1) = 1;
+    case 7
+        xL = zeros(Lg+Lh-1,1); xL(1) = 1;
+        xR = zeros(Lg+Lh-1,1); xR(4) = 1;%% delay of 3 samples ITD
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(4) =1; %% delay of 3 samples ITD
+        xRB = zeros(Lg+Lh-1,1); xRB(1) = 1;
+    case 8
+        Ltot =300;
+        xL = zeros(Lg+Lh-1,1); xL(1:Ltot) = HRTF(1:Ltot,1);
+        xR = zeros(Lg+Lh-1,1); xR(1:Ltot) = HRTF(1:Ltot,2);
+        
+        xLB = zeros(Lg+Lh-1,1); xLB(1:Ltot) =HRTF(1:Ltot,2); 
+        xRB = zeros(Lg+Lh-1,1); xRB(1:Ltot) = HRTF(1:Ltot,1);
 end
 
 
@@ -87,16 +112,16 @@ for k = 1:1:M
     C = [RIR_sources(:,mic_synth(2),k); zeros(Lg-1,1)];
     R = [C(1) zeros(1,Lg-1)];
     H_k_right = toeplitz(C,R);
-
+    
     H(:,1+(k-1)*size(H_k_left,2):k*size(H_k_left,2)) = [H_k_left ; H_k_right];
-	
+    
 end
 
 %% Removing 0 rows
 index_rows_zeros = (H==0);
 sum_of_index = sum(index_rows_zeros,2);
 index_2 = (sum_of_index==M*Lg);
-H(index_2,:) =[]; 
+H(index_2,:) =[];
 x(index_2) = [];
 xB(index_2) = [];
 
@@ -111,15 +136,15 @@ gB = H\xB;
 synth_errorB = norm(H*gB-xB)
 
 
-%% Filtering 
+%% Filtering
 
 SourceFile = {'speech1.wav'};%, 'speech2.wav'};
 NoiseFile = {}; %'White_noise1.wav', 'Babble_noise1.wav'};
 flag_output = 1;
 flag_input = 1;
 
-mic = []; 
-[mic, ~, ~] = computeMicSig4(computed_rir,10,flag_output,flag_input,SourceFile, NoiseFile,g,Lg, mic_listen); 
+mic = [];
+[mic, ~, ~] = computeMicSig4(computed_rir,10,flag_output,flag_input,SourceFile, NoiseFile,g,Lg, mic_listen);
 
 binaural_sig_expected = [fftfilt(xL,source_speech1,samplesToKeep) fftfilt(xR,source_speech1,samplesToKeep) ];
 binaural_sig_with_g = [mic(:,1) mic(:,2)];
@@ -130,8 +155,8 @@ NoiseFile = {}; %'White_noise1.wav', 'Babble_noise1.wav'};
 flag_output = 1;
 flag_input = 1;
 
-micB = []; 
-[micB, ~, ~] = computeMicSig4(computed_rir,10,flag_output,flag_input,SourceFile, NoiseFile,gB,Lg, mic_listen); 
+micB = [];
+[micB, ~, ~] = computeMicSig4(computed_rir,10,flag_output,flag_input,SourceFile, NoiseFile,gB,Lg, mic_listen);
 
 binaural_sig_expectedB = [fftfilt(xLB,source_speech1B,samplesToKeep) fftfilt(xRB,source_speech1B,samplesToKeep) ];
 binaural_sig_with_gB = [micB(:,1) micB(:,2)];
@@ -143,14 +168,15 @@ bin_with_g = binaural_sig_with_g + binaural_sig_with_gB;
 
 radiusOfSweetSpot = 340 * M / (fs_resample*2*exp(1)*pi)
 
-%% Plot results 
-% figure 
-% plot(x,'r');
-% hold on 
-% plot(H*g,'b');
-% 
+%% Plot results
+figure
+plot(x,'r');
+hold on
+plot(H*g,'b');
+legend('x','H*g')
+%
 % figure, spy(H)
-% soundsc(binaural_sig,8e3)
+%soundsc(binaural_sig,8e3)
 
 
 
